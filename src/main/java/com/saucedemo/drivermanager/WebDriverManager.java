922c663f2c3f06a5cb8995a5d89f20e41d159a4e
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2023-01-29 01:37:32 +0100

    cucumber code

---
+++ src/main/java/com/saucedemo/drivermanager/WebDriverManager.java
@@ -0,0 +1,46 @@
+//package com.saucedemo.drivermanager;
+//
+//import java.util.List;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//
+//public class WebDriverManager {
+//
+//	public static WebDriver chromedriver() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public static Object firefoxdriver() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public Object manage() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public Object switchTo() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void get(String baseUrl) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public WebElement findElement(By by) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public List<WebElement> findElements(By by) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//}
